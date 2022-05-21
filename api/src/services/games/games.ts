e8437f3ffc9f20bfaa61764590ddac46d7ccfa1c
Mitch Stark <MitchStark10@gmail.com>
2022-05-21 18:33:36 -0400

    spacing updates

--- api/src/services/games/games.ts
+++ api/src/services/games/games.ts
@@ -1,42 +1,40 @@
-import { db } from 'src/lib/db'
+import { db } from 'src/lib/db';
 import type {
-  QueryResolvers,
-  MutationResolvers,
-  GameResolvers,
-} from 'types/graphql'
+    GameResolvers, MutationResolvers, QueryResolvers
+} from 'types/graphql';
 
 export const games: QueryResolvers['games'] = () => {
-  return db.game.findMany()
-}
+    return db.game.findMany();
+};
 
 export const game: QueryResolvers['game'] = ({ id }) => {
-  return db.game.findUnique({
-    where: { id },
-  })
-}
+    return db.game.findUnique({
+        where: { id },
+    });
+};
 
 export const createGame: MutationResolvers['createGame'] = ({ input }) => {
-  return db.game.create({
-    data: input,
-  })
-}
+    return db.game.create({
+        data: input,
+    });
+};
 
 export const updateGame: MutationResolvers['updateGame'] = ({ id, input }) => {
-  return db.game.update({
-    data: input,
-    where: { id },
-  })
-}
+    return db.game.update({
+        data: input,
+        where: { id },
+    });
+};
 
 export const deleteGame: MutationResolvers['deleteGame'] = ({ id }) => {
-  return db.game.delete({
-    where: { id },
-  })
-}
+    return db.game.delete({
+        where: { id },
+    });
+};
 
 export const Game: GameResolvers = {
-  homeTeam: (_obj, { root }) =>
-    db.game.findUnique({ where: { id: root.id } }).homeTeam(),
-  awayTeam: (_obj, { root }) =>
-    db.game.findUnique({ where: { id: root.id } }).awayTeam(),
-}
+    homeTeam: (_obj, { root }) =>
+        db.game.findUnique({ where: { id: root.id } }).homeTeam(),
+    awayTeam: (_obj, { root }) =>
+        db.game.findUnique({ where: { id: root.id } }).awayTeam(),
+};
--- api/src/services/teams/teams.ts
+++ api/src/services/teams/teams.ts
@@ -1,44 +1,42 @@
-import { db } from 'src/lib/db'
+import { db } from 'src/lib/db';
 import type {
-  QueryResolvers,
-  MutationResolvers,
-  TeamResolvers,
-} from 'types/graphql'
+    MutationResolvers, QueryResolvers, TeamResolvers
+} from 'types/graphql';
 
 export const teams: QueryResolvers['teams'] = () => {
-  return db.team.findMany()
-}
+    return db.team.findMany();
+};
 
 export const team: QueryResolvers['team'] = ({ id }) => {
-  return db.team.findUnique({
-    where: { id },
-  })
-}
+    return db.team.findUnique({
+        where: { id },
+    });
+};
 
 export const createTeam: MutationResolvers['createTeam'] = ({ input }) => {
-  return db.team.create({
-    data: input,
-  })
-}
+    return db.team.create({
+        data: input,
+    });
+};
 
 export const updateTeam: MutationResolvers['updateTeam'] = ({ id, input }) => {
-  return db.team.update({
-    data: input,
-    where: { id },
-  })
-}
+    return db.team.update({
+        data: input,
+        where: { id },
+    });
+};
 
 export const deleteTeam: MutationResolvers['deleteTeam'] = ({ id }) => {
-  return db.team.delete({
-    where: { id },
-  })
-}
+    return db.team.delete({
+        where: { id },
+    });
+};
 
 export const Team: TeamResolvers = {
-  Prediction: (_obj, { root }) =>
-    db.team.findUnique({ where: { id: root.id } }).Prediction(),
-  homeTeamGames: (_obj, { root }) =>
-    db.team.findUnique({ where: { id: root.id } }).homeTeamGames(),
-  awayTeamGames: (_obj, { root }) =>
-    db.team.findUnique({ where: { id: root.id } }).awayTeamGames(),
-}
+    Prediction: (_obj, { root }) =>
+        db.team.findUnique({ where: { id: root.id } }).Prediction(),
+    homeTeamGames: (_obj, { root }) =>
+        db.team.findUnique({ where: { id: root.id } }).homeTeamGames(),
+    awayTeamGames: (_obj, { root }) =>
+        db.team.findUnique({ where: { id: root.id } }).awayTeamGames(),
+};
