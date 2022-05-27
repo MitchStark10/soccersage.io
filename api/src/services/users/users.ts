38ec2779f15b8097a724b5859818e87bc98f8065
Mitch Stark <MitchStark10@gmail.com>
2022-05-26 22:23:41 -0400

    more auth generation

--- api/src/services/users/users.ts
+++ api/src/services/users/users.ts
@@ -1,14 +1,6 @@
-import {
-    AuthenticationError,
-    RedwoodGraphQLError,
-} from '@redwoodjs/graphql-server';
+import { RedwoodGraphQLError } from '@redwoodjs/graphql-server';
 import { db } from 'src/lib/db';
-import {
-    comparePasswordToHash,
-    encryptPassword,
-    generateKey,
-} from 'src/lib/encryption-utils';
-import { logger } from 'src/lib/logger';
+import { encryptPassword } from 'src/lib/encryption-utils';
 import {
     MutationResolvers,
     QueryResolvers,
@@ -19,17 +11,12 @@ export const users: QueryResolvers['users'] = () => {
     return db.user.findMany();
 };
 
-export const user: QueryResolvers['user'] = ({ id, sessionCookie }) => {
+export const user: QueryResolvers['user'] = ({ id }) => {
     if (id) {
         return db.user.findUnique({
             where: { id },
         });
-    } else if (sessionCookie) {
-        return db.user.findUnique({
-            where: { sessionCookie },
-        });
     }
-
     throw new RedwoodGraphQLError(
         'You must provide either an id or a sessionCookie'
     );
@@ -39,9 +26,8 @@ export const createUser: MutationResolvers['createUser'] = async ({
     input: { password, ...rest },
 }) => {
     const hashedPassword = await encryptPassword(password);
-    const sessionCookie = generateKey();
     const graphqlResult = await db.user.create({
-        data: { hashedPassword, status: 'active', sessionCookie, ...rest },
+        data: { hashedPassword, status: 'active', ...rest },
     });
 
     // TODO: set the cookie
@@ -60,43 +46,6 @@ export const deleteUser: MutationResolvers['deleteUser'] = ({ id }) => {
     return db.user.delete({
         where: { id },
     });
-};
-
-export const login: MutationResolvers['login'] = async ({
-    email,
-    password,
-}) => {
-    console.log('Received!');
-
-    const user = await db.user.findUnique({
-        where: { email },
-    });
-
-    if (!user) {
-        logger.debug('Unable to find user with email: ' + email);
-        throw new AuthenticationError('Incorrect username or password.');
-    }
-
-    console.log('comparing', { password, hashedPass: user.hashedPassword });
-
-    if (await comparePasswordToHash(password, user.hashedPassword)) {
-        // The user's email and password matched, so generate the new session cookie and return the user info
-        const sessionCookie = generateKey();
-
-        await db.user.update({
-            data: { sessionCookie },
-            where: { id: user.id },
-        });
-
-        return user;
-    }
-
-    logger.debug('Passwords did not match');
-    throw new AuthenticationError('Incorrect username or password.');
-};
-
-export const signOut: MutationResolvers['signOut'] = ({ email }) => {
-    return db.user.update({ data: { sessionCookie: '' }, where: { email } });
 };
 
 export const User: UserResolvers = {
