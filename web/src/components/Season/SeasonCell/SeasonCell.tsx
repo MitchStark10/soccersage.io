84ab8c489bf89c85d9a558ff1d9d3ff06a81778c
Mitch Stark <MitchStark10@gmail.com>
2022-07-03 13:23:35 -0400

    hook up query to frontend

--- web/src/components/Season/SeasonCell/SeasonCell.tsx
+++ web/src/components/Season/SeasonCell/SeasonCell.tsx
@@ -1,27 +1,27 @@
-import type { FindSeasonById } from 'types/graphql'
-import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
+import type { FindSeasonById } from 'types/graphql';
+import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web';
 
-import Season from 'src/components/Season/Season'
+import Season from 'src/components/Season/Season';
 
 export const QUERY = gql`
-  query FindSeasonById($id: Int!) {
-    season: season(id: $id) {
-      id
-      name
-      startDate
-      endDate
+    query FindSeasonById($id: Int!) {
+        season: season(id: $id) {
+            id
+            name
+            startDate
+            endDate
+        }
     }
-  }
-`
+`;
 
-export const Loading = () => <div>Loading...</div>
+export const Loading = () => <div>Loading...</div>;
 
-export const Empty = () => <div>Season not found</div>
+export const Empty = () => <div>Season not found</div>;
 
 export const Failure = ({ error }: CellFailureProps) => (
-  <div className="rw-cell-error">{error.message}</div>
-)
+    <div className="rw-cell-error">{error.message}</div>
+);
 
 export const Success = ({ season }: CellSuccessProps<FindSeasonById>) => {
-  return <Season season={season} />
-}
+    return <Season season={season} />;
+};
--- web/src/pages/StandingsPage/StandingsPage.tsx
+++ web/src/pages/StandingsPage/StandingsPage.tsx
@@ -1,7 +1,25 @@
+import { useQuery } from '@apollo/client';
 import { H1 } from 'src/components/Core/Text/H1';
 
+const STANDINGS_QUERY = gql`
+    query Standings($seasonId: Int!) {
+        standings(seasonId: $seasonId) {
+            userIdRankings {
+                userId
+                score
+            }
+        }
+    }
+`;
+
 const StandingsPage = () => {
-    return <H1>Standings</H1>;
+    const { data } = useQuery(STANDINGS_QUERY, { variables: { seasonId: 1 } });
+    return (
+        <>
+            <H1>Standings</H1>
+            {JSON.stringify(data)}
+        </>
+    );
 };
 
 export default StandingsPage;
