e173cd4a6c8baa0f0b307cd0596281c18fdcf9de
Mitch Stark <MitchStark10@gmail.com>
2022-07-21 01:07:27 +0000

    remove builder, build home page by myself

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,24 +1,10 @@
-import builder, { BuilderComponent } from '@builder.io/react';
 import { MetaTags } from '@redwoodjs/web';
-import { useEffect, useState } from 'react';
-import { BUILDER_PUBLIC_KEY } from 'src/constants';
-
-builder.init(BUILDER_PUBLIC_KEY);
 
 const HomePage = () => {
-    const [content, setContent] = useState(undefined);
-
-    useEffect(() => {
-        builder.get('page', { url: '/' }).toPromise().then(setContent);
-    }, []);
-
-    console.log('content', content);
     return (
         <>
             <MetaTags title="Home" description="Home page" />
-            {content ? (
-                <BuilderComponent model="home-page" content={content} />
-            ) : null}
+            TODO: Build home page content
         </>
     );
 };
