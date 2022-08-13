72190bc18d49e7cff898044b430314a267dc6ba2
Mitch Stark <MitchStark10@gmail.com>
2022-08-13 16:16:36 +0000

    began working on admin dropdown nav links

--- web/src/layouts/Layout/Header/HeaderLink.tsx
+++ web/src/layouts/Layout/Header/HeaderLink.tsx
@@ -1,15 +1,47 @@
 import { Link } from '@redwoodjs/router';
+import classNames from 'classnames';
+import { useState } from 'react';
+
+const Header_LINK_STYLES =
+    'hover:bg-secondary flex justify-center items-center rounded p-5';
 
 export const HeaderLink: React.FC<React.ComponentProps<typeof Link>> = ({
     to,
     children,
 }) => {
     return (
-        <Link
-            to={to}
-            className="hover:bg-secondary flex justify-center items-center rounded p-5"
-        >
+        <Link to={to} className={Header_LINK_STYLES}>
             {children}
         </Link>
+    );
+};
+interface DropDownHeaderLinksProps {
+    label: string;
+}
+
+export const DropDownHeaderLinks: React.FC<DropDownHeaderLinksProps> = ({
+    label,
+    children,
+}) => {
+    const [isHidden, setIsHidden] = useState(true);
+
+    const showLinks = () => setIsHidden(true);
+    const hideLinks = () => setIsHidden(false);
+
+    return (
+        <span
+            className="relative"
+            onMouseEnter={showLinks}
+            onMouseLeave={hideLinks}
+        >
+            <p className={Header_LINK_STYLES}>{label}</p>
+            <div
+                className={classNames('absolute mt-5 bg-blue', {
+                    hidden: isHidden,
+                })}
+            >
+                {children}
+            </div>
+        </span>
     );
 };
--- web/src/layouts/Layout/Header/NavLinks.tsx
+++ web/src/layouts/Layout/Header/NavLinks.tsx
@@ -1,13 +1,26 @@
+import { useAuth } from '@redwoodjs/auth';
 import { routes } from '@redwoodjs/router';
-import { HeaderLink } from './HeaderLink';
+import { DropDownHeaderLinks, HeaderLink } from './HeaderLink';
 
 export const NavLinks: React.VFC = () => {
+    const { isAuthenticated, hasRole } = useAuth();
+
     return (
         <div className="h-full sticky flex justify-start items-center gap-2">
             <HeaderLink to={routes.home()}>TODO: Add Logo</HeaderLink>
             <HeaderLink to={routes.games()}>Games</HeaderLink>
             <HeaderLink to={routes.predictions()}>Predictions</HeaderLink>
             <HeaderLink to={routes.standings()}>Standings</HeaderLink>
+            {isAuthenticated && hasRole('admin') ? (
+                <DropDownHeaderLinks label="Admin">
+                    <HeaderLink to={routes.adminSeasons()}>Seasons</HeaderLink>
+                    <HeaderLink to={routes.adminTeams()}>Teams</HeaderLink>
+                    <HeaderLink to={routes.adminGames()}>Games</HeaderLink>
+                    <HeaderLink to={routes.adminPredictions()}>
+                        Predictions
+                    </HeaderLink>
+                </DropDownHeaderLinks>
+            ) : null}
         </div>
     );
 };
