d2771204b7b85495cd407ff499f96249bb90cc30
Mitch Stark <mitch.stark@tekton.com>
2022-10-18 21:40:30 -0400

    prevent seed script from wiping data

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,14 +1,8 @@
-import { db } from 'api/src/lib/db';
-
 export default async () => {
     try {
         console.log(
             "\nUsing the default './scripts/seed.{js,ts}' template\nEdit the file to add seed data\n"
         );
-
-        await db.prediction.deleteMany();
-        await db.game.deleteMany();
-        await db.user.deleteMany();
     } catch (error) {
         console.warn('Please define your seed data.');
         console.error(error);
