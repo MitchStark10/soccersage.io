d90a5f6546ed3dc7b70b7d25db6976f2ffcebb70
Mitch Stark <mitch.stark@tekton.com>
2022-12-18 19:02:41 -0600

    enforce CounterNumber min width on mobile

--- web/src/components/Core/Counter/CounterNumber.tsx
+++ web/src/components/Core/Counter/CounterNumber.tsx
@@ -1,6 +1,6 @@
 export const CounterNumber: React.FC = ({ children }) => {
     return (
-        <span className="mr-2 flex h-12 w-12 items-center justify-center rounded-xl bg-primary text-2xl text-white">
+        <span className="mr-2 flex h-12 w-12 min-w-[3rem] items-center justify-center rounded-xl bg-primary text-2xl text-white">
             {children}
         </span>
     );
