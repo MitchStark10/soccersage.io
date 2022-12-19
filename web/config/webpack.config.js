36e3fec9d87fe4cd790b51660a883900f1d34245
Mitch Stark <mitch.stark@tekton.com>
2022-12-18 20:28:08 -0600

    package.json & import updates

--- web/config/webpack.config.js
+++ web/config/webpack.config.js
@@ -1,14 +1,9 @@
 /** @returns {import('webpack').Configuration} Webpack Configuration */
 module.exports = (config, { mode }) => {
-  if (mode === 'development') {
-    // Add dev plugin
-  }
+    if (mode === 'development') {
+        // Add dev plugin
+        config.devServer.allowedHosts = 'all';
+    }
 
-  // Add custom rules for your project
-  // config.module.rules.push(YOUR_RULE)
-
-  // Add custom plugins for your project
-  // config.plugins.push(YOUR_PLUGIN)
-
-  return config
-}
+    return config;
+};
--- web/src/layouts/Layout/Footer.tsx
+++ web/src/layouts/Layout/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
     return (
-        <footer className="mt-5 bottom-0 bg-black text-white p-5 w-full">
+        <footer className="mt-5 bg-black text-white p-5 w-full">
             &copy; Mitch Stark {new Date().getFullYear()}
         </footer>
     );
