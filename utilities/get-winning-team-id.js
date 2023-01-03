b6bea76ac2f0d469ee45437d4204ae1c55e19b8b
Mitch Stark <mitch.stark@tekton.com>
2023-01-03 10:14:10 -0600

    explanatory comment

--- utilities/get-winning-team-id.js
+++ utilities/get-winning-team-id.js
@@ -1,3 +1,5 @@
+// CommonJS is utilized because that seems to be the only way to satisfy the build
+// steps of both the api and web submodules.
 module.exports = {
     getWinningTeamId: (game) => {
         if (game.homeTeamScore > game.awayTeamScore) {
