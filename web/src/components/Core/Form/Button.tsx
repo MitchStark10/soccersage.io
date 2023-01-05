e91233d73a1440715ae6a1c7c15ebbd1c926d4ad
Mitch Stark <MitchStark10@gmail.com>
2023-01-04 20:50:23 -0600

    consistent logo hover behavior

--- web/src/components/Core/Form/Button.tsx
+++ web/src/components/Core/Form/Button.tsx
@@ -34,7 +34,7 @@ export const Button: React.FC<ButtonProps> = (props) => {
         {
             'text-white bg-primary focus:ring-gray':
                 props.variant === 'primary',
-            ' text-black bg-white border-gray hover:bg-secondary hover:text-white hover:border-secondary focus:ring-gray border':
+            ' text-black bg-white border-gray hover:border-secondary-dark hover:text-secondary-dark focus:ring-gray border':
                 props.variant === 'secondary',
             'opacity-75': isDisabled,
         },
