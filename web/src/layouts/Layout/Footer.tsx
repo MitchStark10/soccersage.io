45aa166e2825cfd84f4c6ac935e545359d35c29f
Mitch Stark <MitchStark10@gmail.com>
2023-02-26 18:09:34 -0600

    provide feedback link in footer

--- web/src/layouts/Layout/Footer.tsx
+++ web/src/layouts/Layout/Footer.tsx
@@ -1,7 +1,15 @@
+import { Link, routes } from '@redwoodjs/router';
+
 export const Footer = () => {
     return (
-        <footer className="bg-black text-white p-5 w-full">
+        <footer className="bg-black text-white p-5 w-full flex justify-between items-center">
             &copy; Mitch Stark {new Date().getFullYear()}
+            <Link
+                to={routes.provideFeedback()}
+                className=" font-bold text-white underline"
+            >
+                Provide Feedback
+            </Link>
         </footer>
     );
 };
